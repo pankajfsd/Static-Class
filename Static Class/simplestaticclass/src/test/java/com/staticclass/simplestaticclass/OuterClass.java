9c62049063b617b187bbceed8ffea7fd7c27a9bb
pankajfsd <pankaj.prakash988@gmail.com>
2023-06-22 13:55:53 +0530

    Static Class

--- Static Class/simplestaticclass/src/test/java/com/staticclass/simplestaticclass/OuterClass.java
+++ Static Class/simplestaticclass/src/test/java/com/staticclass/simplestaticclass/OuterClass.java
@@ -1,19 +1,19 @@
 package com.staticclass.simplestaticclass;
 
 public class OuterClass {
-    private static int outerData = 10;
-    public static class NestedClass {
-        private int nestedData = 30;
+	private static int outerData = 10;
 
-        public void displayData() {
-            System.out.println("Outer data: " + outerData);
-            System.out.println("Nested data: " + nestedData);
-        }
-    }
+	public static class NestedClass {
+		private int nestedData = 30;
 
-    public static void main(String[] args) {
-        OuterClass.NestedClass nestedObj = new OuterClass.NestedClass();
-        nestedObj.displayData();
-    }
+		public void displayData() {
+			System.out.println("Outer data: " + outerData);
+			System.out.println("Nested data: " + nestedData);
+		}
+	}
+
+	public static void main(String[] args) {
+		OuterClass.NestedClass nestedObj = new OuterClass.NestedClass();
+		nestedObj.displayData();
+	}
 }
-
